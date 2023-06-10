58b4e6181b31dc5c158e545603b7499b604122a1
Ali Hussnain Mi6 <haideraqeel86@hotmail.com>
2023-06-10 13:06:06 +0500

    Update post-car.js

--- src/api/post-car.js
+++ src/api/post-car.js
@@ -41,6 +41,7 @@ export default async function doPostCar(carDetails, accessToken) {
         console.log(response.data)
         return response.data;
     } catch (error) {
+        console.log(error);
         return null;
     }
 }
